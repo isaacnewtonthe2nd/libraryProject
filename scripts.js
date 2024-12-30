387a7d962b089839b666e2612018f83cfce5d809
isaacnewtonthe2nd <ayotuns73@gmail.com>
2024-12-29 16:15:23 -0800

    Add a displayBooks function that displays newly added books and styles them.

--- scripts.js
+++ scripts.js
@@ -14,4 +14,30 @@ function addBookToLibrary (title, author, pages, status) {
 
 addBookToLibrary('Astronomy for people in a hurry', `Neil De'Grasse Tyson`, '244', 'Not read yet');
 
-addBookToLibrary('Cosmos', 'Carl Sagan', '432', 'not read yet');
+addBookToLibrary('Cosmos', 'Carl Sagan', '432', 'Not read yet');
+
+const cardContainer = document.querySelector(".card-container")
+
+function displayBooks (library) {
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
+    const card = document.createElement("div");
+    card.innerHTML = `
+      <div class="book-name">
+          <p class="title">${book.title}</p>
+          <p class="author">by: ${book.author} </p>
+      </div>
+      <div class="book-info">
+          <p class="page-count">Page Count: ${book.pages} </p>
+          <p class="status">${book.status} </p>
+      </div>
+    `;
+    card.classList.add("card");
+    cardContainer.appendChild(card);
+    book.id = i + 1;
+    console.log(book.id);
+
+  }
+}
+
+displayBooks(myLibrary);
